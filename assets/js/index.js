22bcfbdf7f34a7778f65c9d1b701124dae7b43d8
Fractured2K <fractured2k@gmail.com>
2019-02-05 12:38:51 -0800

    Implemented a carousel for the featured listing cards and gathered content/changed styles for certain components

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // Slick slider
+    $('.featured-products').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+    });
+});
