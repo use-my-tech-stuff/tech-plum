d3037a3bca75b550b44e04c05512d521a9a1f9a2
Fractured2K <fractured2k@gmail.com>
2019-02-05 14:16:43 -0800

    Created review section and a review card component

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
-    // Slick slider
+    // featured listings carousel
     $('.featured-products').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2500,
+        arrows: false
+    });
+
+    $('.reviews-content').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: false
     });
 });
