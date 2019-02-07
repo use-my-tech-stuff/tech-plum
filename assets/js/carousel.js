70f89d08387472d877699159191b7357ad8785b6
Fractured2K <fractured2k@gmail.com>
2019-02-07 12:17:20 -0800

    Rearranged js and changed file names

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -41,12 +41,3 @@ $(document).ready(function () {
         }]
     });
 });
-
-// Mobile navigation
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav-items');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('is-active');
-    nav.classList.toggle('active-flex')
-})
